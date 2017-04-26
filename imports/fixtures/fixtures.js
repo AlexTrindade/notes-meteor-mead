67c3ca9488d7251a88b29b79af96e5942a5de48b
Alex Trindade <trindadealex@gmail.com>
2017-04-26 17:10:44 -0300

    Integrate selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+  {
+    _id: 'noteid1',
+    title: 'Title 1',
+    body: '',
+    updateAt: 1493154969124,
+    userId: 'user1'
+  }, {
+    _id: 'noteid2',
+    title: '',
+    body: 'Body 2',
+    updateAt: 1493154969124,
+    userId: 'user2'
+  }
+];
--- imports/ui/NoteList.test.js
+++ imports/ui/NoteList.test.js
@@ -3,24 +3,8 @@ import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
 
+import {notes} from '../fixtures/fixtures';
 import {NoteList} from './NoteList';
-
-
-const notes = [
-  {
-    _id: 'noteid1',
-    title: 'Title 1',
-    body: '',
-    updateAt: 0,
-    userId: 'user1'
-  }, {
-    _id: 'noteid2',
-    title: '',
-    body: 'Body 2',
-    updateAt: 0,
-    userId: 'user2'
-  }
-];
 
 if (Meteor.isClient) {
   describe('Notelist', () => {
