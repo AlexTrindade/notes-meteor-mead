444667355d3e357eba8ec6a62a43f391c5993496
Alex Trindade <trindadealex@gmail.com>
2017-04-28 12:38:04 -0300

    Mark selected noted in list

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
 
 import {Notes} from '../api/notes';
 import NoteListHeader from './NoteListHeader';
@@ -9,7 +10,6 @@ import NoteListEmptyItem from './NoteListEmptyItem';
 
 
 export const NoteList = (props) => {
-
   return (
     <div>
       <NoteListHeader />
@@ -28,8 +28,14 @@ NoteList.propTypes = {
 }
 
 export default createContainer(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
   Meteor.subscribe('notes');
   return {
-    notes: Notes.find().fetch()
+    notes: Notes.find().fetch().map((note) => {
+      return {
+        ...note,
+        selected: note._id === selectedNoteId
+      }
+    })
   }
 }, NoteList);
